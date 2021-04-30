21324de44e69f4a884596f5b750e9eeeabf74f2a
luigi009 <luigifanaro@hotmail.com>
2021-04-30 07:56:28 -0300

    Updates

---
+++ precache-manifest.4b893024ffaab39e88dac369b5d4b1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57e482cca8eeee946508",
+    "url": "/static/css/main.a390b4c0.chunk.css"
+  },
+  {
+    "revision": "57e482cca8eeee946508",
+    "url": "/static/js/main.57e482cc.chunk.js"
+  },
+  {
+    "revision": "92414366a1279d7b0835",
+    "url": "/static/css/1.f3355b76.chunk.css"
+  },
+  {
+    "revision": "92414366a1279d7b0835",
+    "url": "/static/js/1.92414366.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "830a6e5e519d5e8a8bb95e6716da5949",
+    "url": "/index.html"
+  }
+];
